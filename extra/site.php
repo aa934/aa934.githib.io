6ed30511017a91da389abcadf512214b92518ee2
aa934 <3960691915@qq.com>
2025-02-23 13:25:43 +0800

    Add files via upload

---
+++ extra/site.php
@@ -0,0 +1,79 @@
+<?php
+
+return array (
+  'name' => '在线签名系统',
+  'beian' => '',
+  'cdnurl' => '',
+  'version' => '1.0.1',
+  'timezone' => 'Asia/Shanghai',
+  'forbiddenip' => '',
+  'languages' => 
+  array (
+    'backend' => 'zh-cn',
+    'frontend' => 'zh-cn',
+  ),
+  'fixedpage' => 'zsdhm',
+  'categorytype' => 
+  array (
+    'jichu' => '基础类',
+  ),
+  'configgroup' => 
+  array (
+    'basic' => '基础配置',
+    'sign' => '签名配置',
+    'tongzhi' => '通知配置',
+    'oss' => '存储配置',
+    'dictionary' => '字典配置',
+  ),
+  'mail_type' => '1',
+  'mail_smtp_host' => 'smtp.qq.com',
+  'mail_smtp_port' => '465',
+  'mail_smtp_user' => '10000',
+  'mail_smtp_pass' => 'password',
+  'mail_verify_type' => '2',
+  'mail_from' => '10000@qq.com',
+  'attachmentcategory' => 
+  array (
+    'category1' => '分类一',
+    'category2' => '分类二',
+    'custom' => '自定义',
+  ),
+  'icon' => '',
+  'dzxz' => '',
+  'buy_url' => '',
+  'token' => '',
+  'isdown' => '1',
+  'plkg' => '1',
+  'plxz' => '100',
+  'sbcs' => '666',
+  'sjs' => '1',
+  'wechatkey' => '',
+  'dhtz' => '0',
+  'zctz' => '0',
+  'qmtz' => '0',
+  'ossconfig' => '1',
+  'tSecretId' => '',
+  'tsecretkey' => '',
+  'tregion' => '',
+  'tbucket' => '',
+  'tcosurl' => '',
+  'aliaccessKeyId' => '',
+  'aliaccessKeySecret' => '',
+  'aliendpoint' => '',
+  'alibucket' => '',
+  'aliossurl' => '',
+  'noinject' => '1,2,3',
+  'jymsg' => '本设备已被禁用
+详情请联系客服',
+  'dqmsg' => '本APP已到期
+请前往续费',
+  'downcs' => '200',
+  'diycss' => '',
+  'lbimg' => 
+  array (
+    0 => '',
+  ),
+  'lburl' => '',
+  'signmax' => '25',
+  'gonggao' => '',
+);
